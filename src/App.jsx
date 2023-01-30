418f5b5e02423745f5d1eaf2c9552797bc84df37
IMJOne <mompapa7@gmail.com>
2023-01-30 16:09:55 +0900

    Print videos title with mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 import SearchHeader from './components/SearchHeader';
+
+// queryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+import VideoCard from '../components/VideoCard';
 
 export default function Videos() {
-  return <h1>Videos 🔥</h1>;
+  const { keyword } = useParams();
+  const {
+    isLoding,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], async () => {
+    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+      .then(response => response.json())
+      .then(data => data.items);
+  });
+
+  return (
+    <>
+      <div>Videos {keyword ? `🔎${keyword}` : '🔥'}</div>
+      {isLoding && <p>Loading...</p>}
+      {error && <p>Error ❌</p>}
+      {videos && (
+        <ul>
+          {videos.map(video => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
