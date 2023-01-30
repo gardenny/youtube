2f13f9d3c6267f6eee085d6846f6db6e0dfe39af
IMJOne <mompapa7@gmail.com>
2023-01-30 16:32:10 +0900

    Use Axios library

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -11,9 +12,9 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(['videos', keyword], async () => {
-    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-      .then(response => response.json())
-      .then(data => data.items);
+    return axios //
+      .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+      .then(response => response.data.items);
   });
 
   return (
